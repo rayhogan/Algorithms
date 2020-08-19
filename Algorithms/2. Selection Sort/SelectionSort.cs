0816fa4db880b161c1197a47ac9269330f83a74d
Ray Hogan <rayhogan@gmail.com>
2020-08-19 22:36:22 +0100

    Added selection sort example on an integer array

---
+++ Algorithms/2. Selection Sort/SelectionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class SelectionSort
+    {
+
+        public static void SortArray(int[] input)
+        {
+            int smallest;
+
+            for(int i= 0; i < input.Length-1;i++)
+            {
+                smallest = i;
+
+                for(int j= i+1;j<input.Length;j++)
+                {
+                    if (input[j] < input[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+
+                //Swap values
+                int temp = input[smallest];
+                input[smallest] = input[i];
+                input[i] = temp;
+
+            }
+
+        }
+
+    }
+}
