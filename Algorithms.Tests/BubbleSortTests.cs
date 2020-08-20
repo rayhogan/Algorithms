021f3a9e0fa5fdca9fcbec05830bd58ed315e1fb
Ray Hogan <rayhogan@gmail.com>
2020-08-20 21:22:31 +0100

    Added bubble sort implementation that sorts an array of integers

---
+++ Algorithms.Tests/BubbleSortTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms;
+using System.Linq;
+using System.Data;
+using System;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestMethod]
+        public void SuccessfulSort()
+        {
+            // Arrange
+            int[] originalArray = new int[] { 45, 34, 99, 3, 102, 62, 12 };
+            int[] expectedArray = new int[] { 3, 12, 34, 45, 62, 99, 102 };
+            int[] copiedArray = new int[originalArray.Length];
+            originalArray.CopyTo(copiedArray, 0);
+            // Act
+            BubbleSort.SortArray(copiedArray);
+            // Assert
+            Assert.IsFalse(originalArray.SequenceEqual(copiedArray));
+            Assert.IsTrue(expectedArray.SequenceEqual(copiedArray));
+
+        }
+    }
+}
---
+++ Algorithms/3. Bubble Sort/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class BubbleSort
+    {
+        public static void SortArray(int[] input)
+        {
+            int temp;
+
+            for(int i=0;i<=input.Length-2;i++)
+            {
+                for(int j=0; j<=input.Length-2;j++)
+                {
+                    if(input[j] > input[j+1])
+                    {
+                        temp = input[j+1];
+                        input[j+1] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+        }
+
+  
+    }
+}
