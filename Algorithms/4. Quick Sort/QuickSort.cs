36761587e0eb00f9b2ec61a8b37c94ee97617011
Ray Hogan <rayhogan@gmail.com>
2020-08-21 20:28:22 +0100

    Quick Sort of an integer array

---
+++ Algorithms/4. Quick Sort/QuickSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class QuickSort
+    {
+        public static void Sort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = SplitArray(input, left, right);
+
+                if (pivot > 1)
+                {
+                    Sort(input, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    Sort(input, pivot + 1, right);
+                }
+            }
+        }
+
+        private static int SplitArray(int[] input, int left, int right)
+        {
+            int pivotPoint = input[left];
+            while (true)
+            {
+
+                while (input[left] < pivotPoint)
+                {
+                    left++;
+                }
+
+                while (input[right] > pivotPoint)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (input[left] == input[right]) return right;
+
+                    int temp = input[left];
+                    input[left] = input[right];
+                    input[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
